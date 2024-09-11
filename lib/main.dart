cf52b7c371c0131a3e7b346dd4c1f38012507182
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-09-11 23:14:04 +0330

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int noteNumber) {
+    Audio.load('assets/note$noteNumber.wav').play();
+  }
+
+  Widget buildKey(int noteNumber, Color keyColor) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(noteNumber);
+        },
+        child: Container(
+          color: keyColor,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
